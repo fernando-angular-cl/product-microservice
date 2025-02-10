2add51d5afcd33446475f19b9db10b81a9a37a18
Fernando-angular-cl <fernando.angular.cl@gmail.com>
2025-02-09 21:38:10 -0300

    creando la estructura de carpetas

---
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-fast',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Heavy Loaders Slow</h1>
+  `
+
+})
+export class HeavyLoadersFastComponent {
+
+}
---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loader',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Users Loaders </h1>
+  `
+})
+
+export class UsersLoaderComponent  {
+  constructor() { }
+
+}
